4a4b82ae5474c0f7f63e6618997e0a36eac9ea19
Deepak Purushotham <dpurushotham136@gmail.com>
2022-02-08 13:54:42 -0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Runtime, Inspector } from '@observablehq/runtime';
+  import notebook from '../c019e733534da627@316.js';
+
+  // reference to mutable speed
+  let speed = { value: 0.1 };
+
+  function draw(node) {
+    const runtime = new Runtime();
+    runtime.module(notebook, Inspector.into(document.body));
+  }
+</script>
+
+<p>
+  Visit <a
+    href="https://observablehq.com/@brunolaranjeira/d3-v6-force-directed-graph-with-directional-straight-arrow@316"
+  >
+    Observable</a
+  > to read the documentation
+</p>
+
+<main>
+  <div use:draw />
+</main>
